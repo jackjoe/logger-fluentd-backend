0531877d8ae7467ff03ad4d1a14451e5c3987381
Pieter Michels <pieter@noort.be>
2018-09-13 14:54:00 +0200

    combine test

--- test/logger_test.exs
+++ test/logger_test.exs
@@ -2,18 +2,14 @@ defmodule LoggerFluentdBackend.LoggerTest do
   use ExUnit.Case, async: true
   require Logger
 
-  # @system_name "test_system_name"
   @port 29123
+  @host "localhost"
 
   setup do
     :ok = Application.put_env(:logger, :backends, [LoggerFluentdBackend.Logger])
-    :ok = Application.put_env(:logger, :logger_fluentd_backend, host: "localhost", port: @port)
+    :ok = Application.put_env(:logger, :logger_fluentd_backend, host: @host, port: @port)
     Application.ensure_started(:logger)
     MockFluentdServer.start(@port, self())
-
-    # on_exit(fn ->
-    #   Application.stop(:logger)
-    # end)
 
     :ok
   end
@@ -22,6 +18,14 @@ defmodule LoggerFluentdBackend.LoggerTest do
     log = "Will send this debugging message"
     Logger.debug(log)
     assert_receive {:ok, message}, 5000
+    assert String.contains?(message, log)
+  end
+
+  test "will send a message to the server" do
+    log = "Hello TCP"
+    LoggerFluentdBackend.Sender.send("", log, @host, @port)
+    assert_receive {:ok, message}, 5000
+    # IO.inspect(Enum.join(for <<c::utf8 <- msg>>, do: <<c::utf8>>))
     assert String.contains?(message, log)
   end
 end
