ecf7404e39d6da94ef736c6c5303cfec99d37c00
Pieter Michels <pieter@noort.be>
2018-09-13 14:59:08 +0200

    refactor

--- test/logger_test.exs
+++ test/logger_test.exs
@@ -6,9 +6,10 @@ defmodule LoggerFluentdBackend.LoggerTest do
   @host "localhost"
 
   setup do
-    :ok = Application.put_env(:logger, :backends, [LoggerFluentdBackend.Logger])
-    :ok = Application.put_env(:logger, :logger_fluentd_backend, host: @host, port: @port)
+    Application.put_env(:logger, :backends, [LoggerFluentdBackend.Logger])
+    Application.put_env(:logger, :logger_fluentd_backend, host: @host, port: @port)
     Application.ensure_started(:logger)
+
     MockFluentdServer.start(@port, self())
 
     :ok
@@ -18,14 +19,6 @@ defmodule LoggerFluentdBackend.LoggerTest do
     log = "Will send this debugging message"
     Logger.debug(log)
     assert_receive {:ok, message}, 5000
-    assert String.contains?(message, log)
-  end
-
-  test "will send a message to the server" do
-    log = "Hello TCP"
-    LoggerFluentdBackend.Sender.send("", log, @host, @port)
-    assert_receive {:ok, message}, 5000
-    # IO.inspect(Enum.join(for <<c::utf8 <- msg>>, do: <<c::utf8>>))
     assert String.contains?(message, log)
   end
 end
--- test/sender_test.exs
+++ test/sender_test.exs
@@ -1,13 +1,13 @@
 defmodule SenderTest do
   use ExUnit.Case
 
-  # test "will send a message to the server" do
-  #   port = 28000
-  #   host = "localhost"
-  #   MockFluentdServer.start(port, self())
-  #   LoggerFluentdBackend.Sender.send("", "Hello TCP", host, port)
-  #   # assert_receive {:ok, msg}, 5000
-  #   # IO.inspect(Enum.join(for <<c::utf8 <- msg>>, do: <<c::utf8>>))
-  #   assert_receive {:ok, "Hello TCP"}, 5000
-  # end
+  test "will send a message to the server" do
+    port = 28000
+    host = "localhost"
+    MockFluentdServer.start(port, self())
+    LoggerFluentdBackend.Sender.send("", "Hello TCP", host, port)
+    # assert_receive {:ok, msg}, 5000
+    # IO.inspect(Enum.join(for <<c::utf8 <- msg>>, do: <<c::utf8>>))
+    assert_receive {:ok, "Hello TCP"}, 5000
+  end
 end
